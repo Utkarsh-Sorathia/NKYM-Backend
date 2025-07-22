ae4ad31fb2da4a6948a2a9abec7b9b28d32c85b9
Utkarsh-Sorathia <83358150+Utkarsh-Sorathia@users.noreply.github.com>
2025-07-22 11:16:22 +0000

    Added role in login response

--- controllers/login.controller.ts
+++ controllers/login.controller.ts
@@ -4,10 +4,11 @@ import jwt from 'jsonwebtoken';
 export const adminLogin = (req: Request, res: Response) => {
   const { username, password } = req.body;
   const { ADMIN_USERNAME, ADMIN_PASSWORD, JWT_SECRET } = process.env;
+  let role = "admin";
 
   if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
-    const token = jwt.sign({ isAdmin: true }, JWT_SECRET as string, { expiresIn: '2h' });
-    res.json({ message:"Admin logged in successfully", token });
+    const token = jwt.sign({ isAdmin: true, role }, JWT_SECRET as string, { expiresIn: '2h' });
+    res.json({ role, message:"Admin logged in successfully", token });
   } else {
     res.status(401).json({ message: 'Invalid credentials' });
   }
